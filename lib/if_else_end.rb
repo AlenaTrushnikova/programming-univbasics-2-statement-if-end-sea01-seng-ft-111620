c35e4bf13b244c64bf31f3a4c5b17fe44c4591b3
Alena Trushnikova <alena.yu.t@gmail.com>
2020-09-17 17:30:25 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,7 +1,8 @@
 # Write your solution here
-run_code_inside = true
-puts "Code before if...end"
-if run_code_inside
-  puts "code inside"
+# current_time = Time.now
+# current_time = current_time.to_i
+if Time.now.to_i % 2 == 0
+  puts "Even!"
+else
+  puts "Odd!"
 end
-puts "Code after if...end"
